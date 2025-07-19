500415dca3c8fd4e3446877ae89378acfd957f8b
jaydip6867 <cdmi.design3@gmail.com>
2025-07-19 23:21:14 +0530

    sidebar design

--- src/doctor/DoctorSidebar.js
+++ src/doctor/DoctorSidebar.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import { MdCalendarMonth, MdDifference, MdDiversity1, MdSpaceDashboard } from 'react-icons/md'
 import { NavLink } from 'react-router-dom'
 
 const DoctorSidebar = () => {
     return (
-        <Col xs={12} sm={3} lg={2} className='text-bg-dark position-sticky top-0 vh-100'>
-            <h4 className='p-2 m-0 border-bottom text-center'>Health Doctor</h4>
-            <div className='sidebar_nav'>
-                <NavLink to={'/doctor/doctordashboard'} className={({ isActive }) => (isActive ? 'active' : '')}>Dashboard</NavLink>
-                <NavLink to={'/doctor/doctorappointment'} className={({ isActive }) => (isActive ? 'active' : '')}>Appointment</NavLink>
-                <NavLink to={'/doctor/doctorsurgery'} className={({ isActive }) => (isActive ? 'active' : '')} >Surgery</NavLink>
-                <NavLink to={'/doctor/calender'} className={({ isActive }) => (isActive ? 'active' : '')}>Calender</NavLink>
+        <Col xs={12} sm={3} lg={2} className='position-sticky top-0 vh-100 p-3 pe-0'>
+            <div className='bg-white h-100 rounded shadow'>
+                <div className='px-3'><h4 className='p-3 m-0 border-bottom text-center'>Health Doctor</h4></div>
+                <div className='sidebar_nav mt-3 ps-3'>
+                    <NavLink to={'/doctor/doctordashboard'} className={({ isActive }) => (isActive ? 'active' : '')}><MdSpaceDashboard /> Dashboard</NavLink>
+                    <NavLink to={'/doctor/doctorappointment'} className={({ isActive }) => (isActive ? 'active' : '')}><MdDifference />Appointment</NavLink>
+                    <NavLink to={'/doctor/doctorsurgery'} className={({ isActive }) => (isActive ? 'active' : '')} ><MdDiversity1 />Surgery</NavLink>
+                    <NavLink to={'/doctor/calender'} className={({ isActive }) => (isActive ? 'active' : '')}><MdCalendarMonth /> Calender</NavLink>
+                </div>
             </div>
         </Col>
     )
--- src/patient/P_Sidebar.js
+++ src/patient/P_Sidebar.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
+import { MdDifference, MdOutlineListAlt, MdSpaceDashboard } from 'react-icons/md'
 import { NavLink } from 'react-router-dom'
 
 const P_Sidebar = () => {
     return (
         <>
-            <Col xs={12} sm={3} lg={2} className='text-bg-dark position-sticky top-0 vh-100'>
-                <h4 className='p-2 m-0 border-bottom text-center'>Health Patient</h4>
-                <div className='sidebar_nav'>
-                    <NavLink to={'/patient/patientdahsboard'} className={({ isActive }) => (isActive ? 'active' : '')}>Dashboard</NavLink>
-                    <NavLink to={'/patient/patientdoctorappointment'} className={({ isActive }) => (isActive ? 'active' : '')}>Add Appointment</NavLink>
-                    <NavLink to={'/patient/appointment'} className={({ isActive }) => (isActive ? 'active' : '')} >View Appointment</NavLink>
-                    {/* <NavLink to={'/doctor/calender'} className={({ isActive }) => (isActive ? 'active' : '')}>Calender</NavLink> */}
+            <Col xs={12} sm={3} lg={2} className='position-sticky top-0 vh-100 p-3 pe-0'>
+                <div className='bg-white h-100 rounded shadow'>
+                    <div className='px-3'><h4 className='p-3 m-0 border-bottom text-center'>Health Patient</h4></div>
+                    <div className='sidebar_nav mt-3 ps-3'>
+                        <NavLink to={'/patient/patientdahsboard'} className={({ isActive }) => (isActive ? 'active' : '')}><MdSpaceDashboard />Dashboard</NavLink>
+                        <NavLink to={'/patient/patientdoctorappointment'} className={({ isActive }) => (isActive ? 'active' : '')}><MdDifference />Add Appointment</NavLink>
+                        <NavLink to={'/patient/appointment'} className={({ isActive }) => (isActive ? 'active' : '')} ><MdOutlineListAlt />View Appointment</NavLink>
+                        {/* <NavLink to={'/doctor/calender'} className={({ isActive }) => (isActive ? 'active' : '')}>Calender</NavLink> */}
+                    </div>
                 </div>
             </Col>
         </>
