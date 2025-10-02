5746c71d7d7c3cc8dd92d324e264179f5f946a83
jaydip6867 <cdmi.design3@gmail.com>
2025-10-02 16:18:45 +0530

    amulance update

--- src/Ambulance/Amb_Sidebar.js
+++ src/Ambulance/Amb_Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Col } from 'react-bootstrap'
-import { MdCalendarMonth, MdCardGiftcard, MdDifference, MdDiversity1, MdLocalHospital, MdSpaceDashboard } from 'react-icons/md'
+import { MdLocalHospital, MdSpaceDashboard } from 'react-icons/md'
 import { NavLink } from 'react-router-dom'
 
 const Amb_dashboard = () => {
@@ -20,7 +20,6 @@ const Amb_dashboard = () => {
                     {/* <NavLink to={'/doctor/doctorappointment'} className={({ isActive }) => (isActive ? 'active' : '')}><MdDifference />Appointment</NavLink> */}
                     {/* <NavLink to={'/doctor/doctorsurgery'} className={({ isActive }) => (isActive ? 'active' : '')} ><MdDiversity1 />Surgery</NavLink> */}
                     {/* <NavLink to={'/doctor/doctorblog'} className={({ isActive }) => (isActive ? 'active' : '')} ><MdCardGiftcard />Blog</NavLink> */}
-                    {/* <NavLink to={'/doctor/calender'} className={({ isActive }) => (isActive ? 'active' : '')}><MdCalendarMonth /> Calender</NavLink> */}
                 </div>
             </div>
         </Col>
